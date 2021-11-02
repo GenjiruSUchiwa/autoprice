9c2b496aa81678e5759b72ab46882b740c8122a8
USER <user@USERs-MacBook-Pro.local>
2021-11-02 07:07:30 +0100

    sidebar

---
+++ src/components/Sidebar/SidebarHelp.js
@@ -0,0 +1,29 @@
+import { QuestionIcon } from "@chakra-ui/icons";
+import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import SidebarHelpImage from "assets/img/SidebarHelpImage.png";
+import IconBox from "components/Icons/IconBox";
+import React from "react";
+
+export function SidebarHelp(props) {
+  const { children, ...rest } = props;
+  return (
+    <Flex
+      borderRadius="15px"
+      flexDirection="column"
+      bgImage={SidebarHelpImage}
+      justifyContent="flex-start"
+      alignItems="start"
+      boxSize="border-box"
+      p="16px"
+      h="170px"
+      w="100%"
+    >
+      <IconBox width="35px" h="35px" bg="white" mb="auto">
+        <QuestionIcon color="teal.300" h="18px" w="18px" />
+      </IconBox>
+      <Text fontSize="sm" color="white" fontWeight="bold">
+        Need help?
+      </Text>
+    </Flex>
+  );
+}
